9a959618b0ee86fb3a9ef502bdb3c0c9f3a63d9b
Annah Gaula Manda <165100703+Gaula26@users.noreply.github.com>
2025-04-20 23:18:42 +0200

    Added payment

---
+++ src/main/java/za/ac/cput/domain/PaymentMethod.java
@@ -0,0 +1,15 @@
+/*
+Author: Annah Gaula  Manda (230164250)
+Date: 20/04/2025
+ */
+
+package za.ac.cput.domain;
+
+public enum PaymentMethod {
+
+    SUCCESSFUL,
+    PENDING,
+    FAILED,
+    CANCELLED,
+
+}
