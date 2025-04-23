17024f079a679fd3a024c0cf981119da15bce7ea
Agnes-99 <Agnesmabusela8@gmail.com>
2025-04-23 17:33:46 +0200

    Tested Customer and Admin in Main

--- src/main/java/za/ac/cput/Main.java
+++ src/main/java/za/ac/cput/Main.java
@@ -1,6 +1,8 @@
 package za.ac.cput;
 
+import za.ac.cput.domain.Admin;
 import za.ac.cput.domain.Customer;
+import za.ac.cput.factory.AdminFactory;
 import za.ac.cput.factory.CustomerFactory;
 
 public class Main {
@@ -9,5 +11,9 @@ public class Main {
 
 //            Customer customer = CustomerFactory.createCustomer(22,"Agnes", "Mabusela", "0711938490", "Agnesmabusela@gmail.com","Pass2025", 0);
 //            System.out.println(customer);
+
+//        Admin admin = AdminFactory.createAdmin(1, "Admin", "Administrator", "admin@gmail.com", "Admin012");
+//        System.out.println(admin);
+
     }
 }
