a93c53ad4d5c8d03a3e4842f736a657e77e89799
Agnes-99 <Agnesmabusela8@gmail.com>
2025-04-23 16:46:52 +0200

    Added Main.java

--- src/main/java/za/ac/cput/Main.java
+++ src/main/java/za/ac/cput/Main.java
@@ -1,8 +1,13 @@
 package za.ac.cput;
 
+import za.ac.cput.domain.Customer;
+import za.ac.cput.factory.CustomerFactory;
+
 public class Main {
 
     public static void main(String[] args) {
 
+//            Customer customer = CustomerFactory.createCustomer(22,"Agnes", "Mabusela", "0711938490", "Agnesmabusela@gmail.com","Pass2025", 0);
+//            System.out.println(customer);
     }
 }
