9a2e6e14f52b28c887063cbf696aad20bcfe2d4e
Tsholo0707 <165083310+Tsholo0707@users.noreply.github.com>
2025-04-30 22:16:05 +0200

    Added ShippingFactory

---
+++ src/main/java/za/ac/cput/factory/ShippingFactory.java
@@ -0,0 +1,35 @@
+/*ShippingFactory.java
+Shipping model Factory class
+Author: Tsholofelo Mabidikane (230018165)
+Date: 23 April 2025
+ */
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Shipping;
+import za.ac.cput.util.Helper;
+
+import java.util.Date;
+
+public class ShippingFactory {
+
+    public static Shipping createShipping(int id, int orderId, String address, String status, Date estimatedDeliveryDate, String trackingNumber) {
+
+        if(Helper.isNullOrEmpty(address) || Helper.isNullOrEmpty(status))
+            return null;
+
+        if(estimatedDeliveryDate == null)
+            return null;
+
+        if(Helper.isNullOrEmpty(trackingNumber))
+            return null;
+
+        return new Shipping.Builder()
+                .setId(id)
+                .setOrderId(orderId)
+                .setAddress(address)
+                .setStatus(status)
+                .setEstimatedDeliveryDate(estimatedDeliveryDate)
+                .setTrackingNumber(trackingNumber)
+                .build();
+    }
+}
