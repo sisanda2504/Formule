144a78267fb724740ed1a1debf78fd2749eb8d70
Agnes-99 <Agnesmabusela8@gmail.com>
2025-05-20 04:06:30 +0200

    added IRepositoy

--- src/main/java/za/ac/cput/repository/IRepository.java
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -1,4 +1,11 @@
 package za.ac.cput.repository;
 
-public class IRepository {
+public interface IRepository <T, ID>{
+
+    T create(T t);
+    T read(ID id);
+    T update(T t);
+    boolean delete(ID id);
+
 }
+
