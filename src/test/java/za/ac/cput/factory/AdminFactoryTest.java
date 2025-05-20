3b51c92862d7fea5c8b3c504edb559833be87312
Agnes-99 <Agnesmabusela8@gmail.com>
2025-05-20 03:56:13 +0200

    Added AdminFactoryTest

--- src/test/java/za/ac/cput/factory/AdminFactoryTest.java
+++ src/test/java/za/ac/cput/factory/AdminFactoryTest.java
@@ -1,7 +1,41 @@
 package za.ac.cput.factory;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import za.ac.cput.domain.Admin;
 
+import static org.junit.jupiter.api.Assertions.*;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AdminFactoryTest {
+
+    private static Admin admin1 = AdminFactory.createAdmin(
+            12,
+            "Admin",
+            "Admin12",
+            "admin12a@gmail.com",
+             "admin120");
+
+    private static Admin admin2 = AdminFactory.createAdmin(
+            12,
+            "Admin",
+            "",
+            "admin13a@gmail.com",
+            "admin130");
+
+    @Test
+    @Order(1)
+    public void testCreateAdmin() {
+        assertNotNull(admin1);
+        System.out.println(admin1.toString());
+    }
+
+    @Test
+    @Order(2)
+    public void testCreateAdmin2() {
+        assertNotNull(admin2);
+        System.out.println(admin2.toString());
+    }
 
 }
