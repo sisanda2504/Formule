9a6aeeacd231b85b891d1bf97a438c00c9ec2cf2
Agnes-99 <Agnesmabusela8@gmail.com>
2025-08-19 21:24:01 +0200

    moved to business folder

--- src/main/java/za/ac/cput/repository/business/ShippingRepository.java
+++ src/main/java/za/ac/cput/repository/business/ShippingRepository.java
@@ -1,9 +1,8 @@
 
-package za.ac.cput.repository;
+package za.ac.cput.repository.business;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import za.ac.cput.domain.Shipping;
+import za.ac.cput.domain.business.Shipping;
 
 import java.util.List;
 
