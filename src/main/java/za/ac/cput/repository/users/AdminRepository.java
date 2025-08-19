d4f845ecbdc31d76a905f1a082598acf1bbb5377
Agnes-99 <Agnesmabusela8@gmail.com>
2025-08-19 21:29:35 +0200

    moved to users package

--- src/main/java/za/ac/cput/repository/users/AdminRepository.java
+++ src/main/java/za/ac/cput/repository/users/AdminRepository.java
@@ -1,8 +1,8 @@
-package za.ac.cput.repository;
+package za.ac.cput.repository.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import za.ac.cput.domain.Admin;
+import za.ac.cput.domain.users.Admin;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
