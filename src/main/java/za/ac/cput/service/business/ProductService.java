19033a1a6061fb20098a64e71da7f57355fb30d0
Agnes-99 <Agnesmabusela8@gmail.com>
2025-08-19 21:24:40 +0200

    moved to business folder

--- src/main/java/za/ac/cput/service/business/ProductService.java
+++ src/main/java/za/ac/cput/service/business/ProductService.java
@@ -1,9 +1,10 @@
-package za.ac.cput.service;
+package za.ac.cput.service.business;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import za.ac.cput.domain.Product;
-import za.ac.cput.repository.ProductRepository;
+import za.ac.cput.domain.business.Product;
+import za.ac.cput.repository.business.ProductRepository;
+
 @Service
 public class ProductService implements IProductService {
     private final ProductRepository repository;
