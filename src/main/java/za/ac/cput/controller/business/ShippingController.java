c5a8ae87f5690d568a96bbcc1d3074ec23c78a31
Agnes-99 <Agnesmabusela8@gmail.com>
2025-08-19 21:24:23 +0200

    moved to business folder

--- src/main/java/za/ac/cput/controller/business/ShippingController.java
+++ src/main/java/za/ac/cput/controller/business/ShippingController.java
@@ -2,12 +2,12 @@
 Author: Tsholofelo Mabidikane (230018165)
 Date: 07 August 2025
  */
-package za.ac.cput.controller;
+package za.ac.cput.controller.business;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import za.ac.cput.service.ShippingService;
-import za.ac.cput.domain.Shipping;
+import za.ac.cput.service.business.ShippingService;
+import za.ac.cput.domain.business.Shipping;
 
 import java.util.List;
 
