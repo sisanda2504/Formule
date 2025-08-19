60f7492b3816d9107117497be86f3266dde31d89
Agnes-99 <Agnesmabusela8@gmail.com>
2025-08-19 21:23:41 +0200

    moved to business folder

--- src/test/java/za/ac/cput/service/business/ShippingServiceTest.java
+++ src/test/java/za/ac/cput/service/business/ShippingServiceTest.java
@@ -2,15 +2,15 @@
 Author: Tsholofelo Mabidikane (230018165)
 Date: 05 August 2025
  */
-package za.ac.cput.service;
+package za.ac.cput.service.business;
 
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import za.ac.cput.domain.Shipping;
-import za.ac.cput.factory.ShippingFactory;
+import za.ac.cput.domain.business.Shipping;
+import za.ac.cput.factory.business.ShippingFactory;
 
 import java.time.LocalDate;
 
