6372653990b948d741258281838ddafbbe3c0367
Agnes-99 <Agnesmabusela8@gmail.com>
2025-08-19 21:28:35 +0200

    moved to users package

--- src/main/java/za/ac/cput/repository/users/CustomerRepository.java
+++ src/main/java/za/ac/cput/repository/users/CustomerRepository.java
@@ -1,8 +1,8 @@
-package za.ac.cput.repository;
+package za.ac.cput.repository.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import za.ac.cput.domain.Customer;
+import za.ac.cput.domain.users.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
