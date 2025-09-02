155c4b0df334b8dfaceed0c0d9a3e042a3c20f97
Agnes-99 <Agnesmabusela8@gmail.com>
2025-09-02 02:00:38 +0200

    updated

--- src/main/java/za/ac/cput/domain/business/Brands.java
+++ src/main/java/za/ac/cput/domain/business/Brands.java
@@ -8,13 +8,22 @@ Date: 18/04/2025
 */
 
 public enum Brands {
-    INNISFREE,
-    MISSHA,
-    LANEIGE,
-    SK_II,
-    SHISEIDO,
-    HADA_LABO,
-    SOME_BY_MI,
-    COSRX,
-    THE_FACE_SHOP
+    INNISFREE("Innisfree"),
+    MISSHA("Missha"),
+    LANEIGE("Laneige"),
+    SK_II("SK-II"),
+    SHISEIDO("Shiseido"),
+    HADA_LABO("Hada Labo"),
+    SOME_BY_MI("Some By Mi"),
+    COSRX("CORSRX"),
+    THE_FACE_SHOP("The Face Shop");
+
+    private final String displayName;
+    private Brands(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
