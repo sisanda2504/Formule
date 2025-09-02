059737a47670a98f493524f2bda89f1df177412c
Agnes-99 <Agnesmabusela8@gmail.com>
2025-09-02 02:00:11 +0200

    fixed

--- src/test/java/za/ac/cput/factory/generic/AddressFactoryTest.java
+++ src/test/java/za/ac/cput/factory/generic/AddressFactoryTest.java
@@ -5,62 +5,66 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import za.ac.cput.domain.generic.Address;
+import za.ac.cput.domain.users.Customer;
+import za.ac.cput.factory.users.CustomerFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AddressFactoryTest {
 
-    private static Address address1 = AddressFactory.createAddress(
-            1,
-            109,
+    private static final Customer customer = CustomerFactory.createCustomer(
+            "Test",
+            "User",
+            "0712345678",
+            "testuser@example.com",
+            "Password123!",
+            null
+    );
+
+    private static Address validAddress = AddressFactory.createAddress(
+            customer,
             "123 Main Street",
             "Cape Town",
             "Western Cape",
             "8001",
             "South Africa"
     );
+    private static Address addressWithNoStreet = AddressFactory.createAddress(
+            customer,
+            "",
+            "Durban",
+            "KwaZulu-Natal",
+            "4001",
+            "South Africa"
+    );;
+    private static Address addressWithNoCity =  AddressFactory.createAddress(
+            customer,
+            "456 Oak Avenue",
+            null,
+            "Gauteng",
+            "2000",
+            "South Africa"
+    );;
 
     @Test
     @Order(1)
     void testCreateAddressSuccess() {
-        assertNotNull(address1);
-        System.out.println(address1.toString());
+        assertNotNull(validAddress);
+        System.out.println(validAddress.toString());
     }
 
     @Test
     @Order(2)
     void testCreateAddressWithEmptyStreet() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            AddressFactory.createAddress(
-                    2,
-                    110,
-                    "",
-                    "Durban",
-                    "KwaZulu-Natal",
-                    "4001",
-                    "South Africa"
-            );
-        });
-        assertEquals("Please provide a street", exception.getMessage());
-        System.out.println("Exception caught: " + exception.getMessage());
+        assertNull(addressWithNoStreet);
+        System.out.println(addressWithNoStreet.toString());
     }
 
     @Test
     @Order(3)
     void testCreateAddressWithNullCity() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            AddressFactory.createAddress(
-                    3,
-                    111,
-                    "456 Oak Avenue",
-                    null,
-                    "Gauteng",
-                    "2000",
-                    "South Africa"
-            );
-        });
-        assertEquals("Please provide a city", exception.getMessage());
-        System.out.println("Exception caught: " + exception.getMessage());
+        assertNull(addressWithNoCity);
+        System.out.println(addressWithNoCity.toString());
     }
 }
